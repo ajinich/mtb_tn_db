514b7f131489ef254dad3fe0ffdcadce03efb329
Anisha Zaveri <42725952+anishazaveri@users.noreply.github.com>
2020-02-08 15:35:16 -0500

    made dash data edited v5, volcano not working

--- dash/tnseq_webapp_v5/app.py
+++
@@ -1,9 +0,0 @@
-import dash
-import dash_core_components as dcc
-
-external_scripts = ['https://code.jquery.com/jquery-3.3.1.min.js',
-                    'https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js']
-external_stylesheets = [dbc.themes.UNITED]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, external_scripts=external_scripts)
-server=app.server
-app.config.suppress_callback_exceptions = True
